cf0db98ace120920c6983424a713bbfcfa16edc2
dykoh05 <dykoh05@gmail.com>
2021-12-08 20:06:49 +0700

    finding common prefix

---
+++ src/test/CommonPrefix.java
@@ -0,0 +1,26 @@
+package test;
+
+public class CommonPrefix {
+	
+	static String calcPrefix(String[] strs) {
+		String prefix = strs[0]; // init prefix
+		if(strs.length == 0) {
+			return "";
+		}
+		for(int i = 0;i<strs.length;i++) {
+			while(!strs[i].startsWith(prefix)) {
+				prefix = prefix.substring(0,prefix.length()-1);
+			}
+		}
+		return prefix;
+	}
+
+	public static void main(String[] args) {
+		String[] strs = new String[]{"aaa","aaa",""};
+		String output = calcPrefix(strs);
+		System.out.println(output);
+		
+
+	}
+
+}
