4b01226913f0f84879792fa155b07d4c703916cf
dykoh05 <dykoh05@gmail.com>
2022-01-05 09:22:46 +0700

    school register

--- src/test/SchoolRegister.java
+++ src/test/SchoolRegister.java
@@ -2,47 +2,28 @@ package test;
 import java.util.*;
 
 public class SchoolRegister {
-	  static void ex12(String[] cashier1, String[] cashier2){
-		    ArrayDeque<String> customer1 = new ArrayDeque<String>();
-		    ArrayDeque<String> customer2 = new ArrayDeque<String>();
-		    ArrayList<String> time = new ArrayList<String>();
-		    double tot_b = 0;
-		    double tot_c = 0;
-		    for(int i = 0;i<3;i++){
-		      customer1.add(cashier1[i]);
-		      customer2.add(cashier2[i]);
+	  static void ex11(String[] bus1, String[] bus2, String[] registry){
+		    ArrayDeque<String> students = new ArrayDeque<String>();
+		    String a = "";
+		    for(int i = 0;i<bus1.length;i++){
+		      students.add(bus1[i]);
+		      students.add(bus2[i]);
 		    }
-		    while(!customer1.isEmpty()){
-		      String d = customer1.poll();
-		      double c = Math.floor((Math.random() * 100) +1);
-		      if(c>60){
-		        time.add(d);
+		    while(!students.isEmpty()){
+		      a = students.poll();
+		      for(int i = 0;i<registry.length;i++){
+		        if(registry[i] == a){
+		          System.out.println(a + " is not absent");
+		          break;
+		        } 
 		      }
-		      tot_b = tot_b + c;
 		    }
-		    while(!customer2.isEmpty()){ // can be optimised
-		      String d = customer2.poll();
-		      double c = Math.floor((Math.random() * 100) +1);
-		      if(c>60){
-		        time.add(d);
-		      }
-		      tot_c = tot_c + c;
-		    }
-		    for(int i = 0;i<time.size();i++){
-		      System.out.println(time.get(i));
-		    }
-		    if(tot_b > tot_c){
-		      System.out.println("CASHIER1 is slower");
-		    }else{
-		      System.out.println("CASHIER2 is slower");
-		    }
-
 		  }
-
 	public static void main(String[] args) {
-	    String[] cashier1 = new String[]{"Roger", "John", "Nikos", "Marion"};
-	    String[] cashier2 = new String[]{"Nora", "Bill", "Eliza", "Takis"};
-	    ex12(cashier1, cashier2);
+	    String[] bus1 = new String[]{"Roger", "John", "Nikos", "Marion", "Hellen"};
+	    String[] bus2 = new String[]{"Nora", "Bill", "Eliza", "Takis", "Alex" };
+	    String[] registry = new String[]{"Alex", "John", "Elina", "Nikos", "Leo", "Marion", "Hellen", "Nora", "Bill", "Eliza", "Takis", "Roger"};
+	    ex11(bus1, bus2, registry);
 
 	}
 
